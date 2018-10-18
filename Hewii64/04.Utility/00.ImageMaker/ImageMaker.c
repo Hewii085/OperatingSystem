9a03e57c12be32a2c412b4dec760f11b12c4a042
bin <kurai085@gmail.com>
2018-10-18 23:18:30 +0900

    .관리 시작

---
+++ Hewii64/04.Utility/00.ImageMaker/ImageMaker.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <io.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+#define BYTESOFSECTOR 512
---
+++ Hewii64/Includes.h
@@ -0,0 +1,13 @@
+/*
+ * Includes.h
+ *
+ *  Created on: 2018. 10. 3.
+ *      Author: Bin
+ */
+
+#ifndef INCLUDES_H_
+#define INCLUDES_H_
+
+
+
+#endif /* INCLUDES_H_ */
