816d47d22ee7116c3eb52e82efcf91eabe09750f
bin <kurai085@gmail.com>
2019-10-06 15:43:16 +0900

    Add Tiemr Device Driver

--- Hewii64/02.Kernel64/Source/ConsoleShell.h
+++ Hewii64/02.Kernel64/Source/ConsoleShell.h
@@ -6,8 +6,8 @@
 #define CONSOLESHELL_MAXCOMMANDBUFFERCOUNT  300
 #define CONSOLESHELL_PROMPTMESSAGE          "MINT64>"
 
+// 문자열 포인터를 파라미터로 받는 함수 포인터 타입 정의
 typedef void ( * CommandFunction ) ( const char* pcParameter );
-
 
 #pragma pack( push, 1 )
 
@@ -32,10 +32,16 @@ void kExecuteCommand( const char* pcCommandBuffer );
 void kInitializeParameter( PARAMETERLIST* pstList, const char* pcParameter );
 int kGetNextParameter( PARAMETERLIST* pstList, char* pcParameter );
 
+// 커맨드를 처리하는 함수
 void kHelp( const char* pcParameterBuffer );
 void kCls( const char* pcParameterBuffer );
 void kShowTotalRAMSize( const char* pcParameterBuffer );
 void kStringToDecimalHexTest( const char* pcParameterBuffer );
 void kShutdown( const char* pcParamegerBuffer );
+void kSetTimer( const char* pcParameterBuffer );
+void kWaitUsingPIT( const char* pcParameterBuffer );
+void kReadTimeStampCounter( const char* pcParameterBuffer );
+void kMeasureProcessorSpeed( const char* pcParameterBuffer );
+void kShowDateAndTime( const char* pcParameterBuffer );
 
 #endif /*__CONSOLESHELL_H__*/
